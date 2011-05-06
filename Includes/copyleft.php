90dce09655619ca14bac211542adee4ab2ee5dcb
sekuline <sekuline@gmail.com>
2011-05-06 21:20:01 +0200

    Correct bug avec blok.php Textbox.

---
+++ Includes/copyleft.php
@@ -0,0 +1,13 @@
+<?php
+// -------------------------------------------------------------------------//
+// Nuked-KlaN - PHP Portal                                                  //
+// http://www.nuked-klan.org                                                //
+// -------------------------------------------------------------------------//
+// This program is free software. you can redistribute it and/or modify     //
+// it under the terms of the GNU General Public License as published by     //
+// the Free Software Foundation; either version 2 of the License.           //
+// -------------------------------------------------------------------------//
+if (!defined("INDEX_CHECK")) exit('You can\'t run this file alone.');
+?>
+<div id="copyleft" style="text-align: center; width: 100%">
+<a href="http://www.nuked-klan.org" onclick="window.open(this.href); return false;"><img style="width: 80px; height: 15; border: 0" src="images/nk_powered.gif" alt="" title="Powered by Nuked-Klan <?php echo $nuked['version']; ?> © 2002, 2011" /></a></div>
