b4bb91770f76bed7c08e7ff67766c01921111d3c
akred <akred@live.com>
2012-08-11 13:02:54 +0200

    Secure variables (calendar & userbox)

--- modules/Calendar/index.php
+++ modules/Calendar/index.php
@@ -302,7 +302,7 @@ function index(){
 function show_event(){
 	global $bgcolor2, $user, $nuked, $theme, $language;
 	
-	if($_REQUEST['type'] == "birthday" && preg_match("`[a-zA-Z0-9]+$`", $_REQUEST['eid'])){
+	if($_REQUEST['type'] == "birthday" && ctype_alnum($_REQUEST['eid'])){
 		$sql = mysql_query("SELECT pseudo, prenom, age FROM " . USER_TABLE . " WHERE id = '" . $_REQUEST['eid'] . "'");
 		list($pseudo, $prenom, $birthday) = mysql_fetch_array($sql);
 		
